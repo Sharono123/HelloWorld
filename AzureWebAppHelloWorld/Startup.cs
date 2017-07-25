fbd15f16a9cc70e80fcf85fe619284d06fd94f58
sholiar <sholiar@SHOLIAR-X1G3>
2017-07-25 12:01:48 +0300

    check in all

---
+++ AzureWebAppHelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWebAppHelloWorld.Startup))]
+namespace AzureWebAppHelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
